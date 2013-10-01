66c36507251c2af8c8ec2e1798db50aecd1a1b7b
Benproductions1 <ben.schaaf@gmail.com>
2013-10-01 19:07:46 +1000

    Fixed Logout for Offline Players

--- Assets/Scripts/Menu/MainMenu.js
+++ Assets/Scripts/Menu/MainMenu.js
@@ -371,6 +371,7 @@ function QuitDropdown() {
     if (GUILayout.Button("Logout")) {
         window = MenuWindow.quickMatch;
         dropdown = Dropdown.none;
+        Settings.offline = false;
         AccountSettings.Reset();
         AccountSettings.Load();
     }
