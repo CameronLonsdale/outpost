d21c2533512b63603ac6879a2953aa2bdbf5e7d4
Benproductions1 <ben.schaaf@gmail.com>
2013-10-01 23:20:58 +1000

    Changed .gitignore to only ignore root Mods folder

---
+++ Assets/Scripts/Mods/Default.js
@@ -0,0 +1,12 @@
+class Default {
+    var map:int;
+    var gameMode:int;
+    
+    var targetKills:int;
+    
+    var startTime:float;
+    var respawnTime:float;
+    var deathTime:float;
+    var winTime:float;
+    var postTime:float;
+}
